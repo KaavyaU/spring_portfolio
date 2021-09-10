fa3f81700fc9de3c773ab8b2ad24a6caca1883ae
DhruvBhatnagar <dhruv.sandiego@gmail.com>
2021-09-10 10:06:39 -0700

    Dhruv.java is hackathon 1, Minilab_Two and thermo are both classes part of hackathon 2.

---
+++ src/main/java/com/example/sping_portfolio/MiniLabs/Dhruv/Minilab_two.java
@@ -0,0 +1,19 @@
+package com.example.sping_portfolio.MiniLabs.Dhruv;
+import java.util.Scanner ;
+
+public class Minilab_two {
+    public static void main(String[] argsv) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What is your age?");
+        int age = keyboard.nextInt();
+        System.out.println("What is your name?");
+        String name = keyboard.next();
+        System.out.println("What is your temperature?");
+        int temp = keyboard.nextInt();
+
+        Thermo t = new Thermo();
+        t.check_temp( age, temp, name);
+    }
+}
+
+
