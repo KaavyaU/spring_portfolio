ed053fb7a0e3cb6e0c7a30898cc612a6240df34e
KaavyaU <ftc13184@gmail.com>
2021-10-10 22:44:18 -0700

    Updating Grayscale Class Layout

--- src/main/java/com/example/sping_portfolio/controllers/Grayscale/GrayscaleImages.java
+++ src/main/java/com/example/sping_portfolio/controllers/Grayscale/GrayscaleImages.java
@@ -5,14 +5,17 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class GrayscaleImages {
-    protected File image;
+public class GrayscaleImages {
     protected List<ImageInfo> imageList;
+    /*
     protected List<ImageProp> imageDetails;
+    protected File image;
     protected String imageDesc;
     protected String base64;
     protected List<Integer> RGB;
-    protected Array rbgVal;
+    protected int [][][] rbgVal;
     protected List<String> hexadecimal;
     protected Array binaryVal;
+
+     */
 }
