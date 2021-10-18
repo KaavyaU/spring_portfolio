9df76e0e41fdc637788172ddfa28fa0e1b49a363
christopher <chrisddur@gmail.com>
2021-10-18 16:23:23 -0700

    ascii image fix

--- src/main/java/com/example/sping_portfolio/controllers/Grayscale/ImagePropRGB.java
+++
@@ -1,9 +0,0 @@
-package com.example.sping_portfolio.controllers.Grayscale;
-
-import java.util.List;
-
-public class ImagePropRGB {
-    private int [][][] rgbAttributes;
-    private List<Integer> hexadecimal;
-    private int [][][] binaryAttributes;
-}
